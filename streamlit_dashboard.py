23d0a36fa32bae54e361f8c0eb9fd333ea6591be
manofmethis <91790352+manofmethis@users.noreply.github.com>
2024-08-18 15:11:17 +0530

    Update streamlit_dashboard.py

--- streamlit_dashboard.py
+++ streamlit_dashboard.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import plotly.express as px
 import pandas as pd
-import seaborn as sns 
-import matplotlib.pyplot as plt
+# import seaborn as sns 
+# import matplotlib.pyplot as plt
 
 file_path = "adsl.xlsx"
 data = pd.read_excel(file_path)
